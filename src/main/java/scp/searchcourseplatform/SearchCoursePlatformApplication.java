4b0b180e80bbf0adc49445c74211d99bbe1c7ff0
DearaBianca <biancadeara1308@gmail.com>
2024-11-24 08:42:22 +0200

    Initial commit

---
+++ src/main/java/scp/searchcourseplatform/SearchCoursePlatformApplication.java
@@ -0,0 +1,13 @@
+package scp.searchcourseplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchCoursePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchCoursePlatformApplication.class, args);
+    }
+
+}
